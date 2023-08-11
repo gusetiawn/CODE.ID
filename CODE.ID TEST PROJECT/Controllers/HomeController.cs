e9e3418db86f1e5665edcf72832284241d66b723
Agus Setiawan <gusetiawn@gmail.com>
2023-08-11 21:01:49 +0700

    commit project

---
+++ CODE.ID TEST PROJECT/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using CODE.ID_TEST_PROJECT.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CODE.ID_TEST_PROJECT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index(int page = 1, int pageSize = 10)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/comments");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var comments = JsonConvert.DeserializeObject<List<Comment>>(content);
+
+                var totalItems = comments.Count;
+                var totalPages = (int)System.Math.Ceiling(totalItems / (double)pageSize);
+
+                var paginatedComments = comments.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+                ViewBag.Page = page;
+                ViewBag.TotalPages = totalPages;
+                ViewBag.PageSize = pageSize;
+
+                return View(paginatedComments);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ CODE.ID TEST PROJECT/Models/PaginatedList.cs
@@ -0,0 +1,25 @@
+﻿namespace CODE.ID_TEST_PROJECT.Models
+{
+    public class PaginatedList<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+        public List<T> Items { get; private set; }
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            Items = items.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+        }
+
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
+
+        public static PaginatedList<T> Create(List<T> source, int pageIndex, int pageSize)
+        {
+            int count = source.Count();
+            return new PaginatedList<T>(source, count, pageIndex, pageSize);
+        }
+    }
+}
